c212504a4807b79ce8a7c57130e9a4e61aa19bdb
writtic <writtic@gmail.com>
2016-08-30 15:30:25 +0900

    clear valuables name

--- src/main/java/com/enow/storm/TriggerTopology/StagingBolt.java
+++ src/main/java/com/enow/storm/TriggerTopology/StagingBolt.java
@@ -25,7 +25,8 @@ import java.util.List;
 public class StagingBolt extends BaseRichBolt {
     protected static final Logger LOG = LoggerFactory.getLogger(CallingKafkaBolt.class);
     private OutputCollector collector;
-    private TopicStructure ts;
+    private TopicStructure topicStructure;
+    String msg = null;
     boolean machineIdCheck = false;
     boolean phaseRoadMapIdCheck = false;
 
@@ -33,47 +34,45 @@ public class StagingBolt extends BaseRichBolt {
 
     public void prepare(Map MongoConf, TopologyContext context, OutputCollector collector) {
         this.collector = collector;
-        ts = new TopicStructure();
+        topicStructure = new TopicStructure();
     }
 
     @Override
     public void execute(Tuple input) {
-
-        if (null == input.getValueByField("topicStucture")) {
+        topicStructure = (TopicStructure) input.getValueByField("topicStucture");
+        msg = input.getStringByField("msg");
+        if (null == topicStructure) {
             return;
-        } else if ((null == input.getStringByField("msg") || input.getStringByField("msg").length() == 0)) {
+        } else if ((null == msg || msg.length() == 0)) {
             return;
         }
-
-        ts = (TopicStructure) input.getValueByField("topicStucture");
-        final String msg = input.getStringByField("msg");
-
+        // check Device ID
         MongoClient mongoClient = new MongoClient("52.193.56.228", 9092);
 
         mongoClient.setWriteConcern(WriteConcern.ACKNOWLEDGED);
         MongoDatabase dbWrite = mongoClient.getDatabase("enow");
         MongoCollection<Document> deviceListCollection = dbWrite.getCollection("device");
 
-        if (deviceListCollection.count(new Document("deviceId", ts.getDeviceId())) == 0) {
+        if (deviceListCollection.count(new Document("deviceId", topicStructure.getDeviceId())) == 0) {
             machineIdCheck = false;
-        } else if (deviceListCollection.count(new Document("deviceId", ts.getDeviceId())) == 1) {
+        } else if (deviceListCollection.count(new Document("deviceId", topicStructure.getDeviceId())) == 1) {
             machineIdCheck = true;
         } else {
-//			machineIdCheck = "device id : now we have a problem";
+			// machineIdCheck = "device id : now we have a problem";
             LOG.debug("There are more than two machine ID on MongoDB");
         }
-
+        // check Phase Road-map ID
         MongoCollection<Document> phaseRoadMapCollection = dbWrite.getCollection("phaseRoadMap");
 
-        if (phaseRoadMapCollection.count(new Document("phaseRoadMapId", ts.getPhaseRoadMapId())) == 0) {
+        if (phaseRoadMapCollection.count(new Document("phaseRoadMapId", topicStructure.getPhaseRoadMapId())) == 0) {
             phaseRoadMapIdCheck = false;
-        } else if (phaseRoadMapCollection.count(new Document("phaseRoadMapId", ts.getPhaseRoadMapId())) == 1) {
+        } else if (phaseRoadMapCollection.count(new Document("phaseRoadMapId", topicStructure.getPhaseRoadMapId())) == 1) {
             phaseRoadMapIdCheck = true;
         } else {
-//			phaseRoadMapIdCheck = "phase road map id : now we have a problem";
+            // phaseRoadMapIdCheck = "phase road map id : now we have a problem";
             LOG.debug("There are more than two Phase Roadmap Id on MongoDB");
         }
-        collector.emit(new Values(ts, msg, machineIdCheck, phaseRoadMapIdCheck));
+        collector.emit(new Values(topicStructure, msg, machineIdCheck, phaseRoadMapIdCheck));
 
         try {
             LOG.debug("input = [" + input + "]");
